351c8f1ab7c7dc95ae6d6eefbe6280eb0a35e93c
Ivana Petrovic <Ivana.Petrovic1@rs.ey.com>
2022-03-01 15:42:44 +0100

    outputing list items dynamically

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 import NewExpense from "./components/NewExpense/Newexpense";
 import Expenses from "./components/Expenses/Expenses";
 
-const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Office supplies",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: "e2",
-      title: "TV Tuner",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Tablet",
-      amount: 450.53,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+const dummyExpenses = [
+  {
+    id: "e1",
+    title: "Office supplies",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "TV Tuner",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Tablet",
+    amount: 450.53,
+    date: new Date(2021, 5, 12),
+  },
+];
 
-  const addExpenseHandler = (expense) => {
-    console.log(expense);
+const App = () => {
+  const [expenses, setExpenses] = useState(dummyExpenses);
+
+  const addExpenseHandler = (newExpense) => {
+    setExpenses((prevExpenses) => {
+      return [newExpense, ...prevExpenses];
+    });
+    // console.log(newExpense);
   };
 
   return (
