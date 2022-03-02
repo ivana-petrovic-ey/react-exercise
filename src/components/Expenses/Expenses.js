e75449eda814baa3f8650357eccdfc6ce1c2feda
Ivana Petrovic <Ivana.Petrovic1@rs.ey.com>
2022-03-02 12:02:59 +0100

    added ChartBar component and logic

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
-import "./Expenses.css";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+import "./Expenses.css";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -11,7 +13,7 @@ const Expenses = (props) => {
   };
 
   const filteredExpenses = props.items.filter((el) => {
-    return el.date.getFullYear().toString() == filteredYear;
+    return el.date.getFullYear().toString() === filteredYear;
   });
 
   return (
@@ -20,6 +22,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
